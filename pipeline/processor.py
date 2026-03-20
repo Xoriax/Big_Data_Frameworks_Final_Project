42edda883a6a137ccb957921f90729e7e4622b87
Xoriax <lucas.tear@efrei.net>
2026-03-20 15:56:22 +0100

    feat: ajouter le traitement des données avec Spark dans processor.py (a modifier)

--- pipeline/processor.py
+++ pipeline/processor.py
@@ -1,1 +1,32 @@
-#fichier pour l'instant vide
+from datetime import date
+from pyspark.sql import SparkSession, functions as F
+import time
+
+
+spark = (
+    SparkSession.builder
+    .appName("processor")
+    .enableHiveSupport()
+    .getOrCreate()
+)
+
+
+input_path = "hdfs://namenode:9000/data/raw/breweries_partitioned" 
+df = (
+    spark.read.parquet(input_path)
+)
+
+
+df2 = (df
+      .withColumn("id", F.col("id").cast("int"))
+      .select("name", "city", "state", "id"))
+
+
+time.sleep(120)
+
+
+(df2.write
+  .mode("overwrite")
+  .format("parquet")
+  .partitionBy("city")
+  .saveAsTable("default.breweries_curated"))
